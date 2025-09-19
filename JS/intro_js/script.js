89714fd592f985e260c694ac9269ba30c2a41ade
M4shh00r4 <mashhooramanha@gmail.com>
2025-09-19 20:53:10 +0600

    intro into js

---
+++ JS/intro_js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("p1").textContent ="we are learning JS"
+//this is js a comments which is important for documentation but it is ignored by js to be excecuted
+//variable
+let number1=21; //type=>number because there is not "" and the value is a number but it is inside ""
+let number2="8";
+
+document.getElementById("p2").textContent = number1+Number(number2);
