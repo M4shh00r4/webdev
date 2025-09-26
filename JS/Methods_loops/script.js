b46fe02ceedbfbdee78a16bf555bb34bcb316712
M4shh00r4 <mashhooramanha@gmail.com>
2025-09-26 20:41:26 +0600

    methods_loops

---
+++ JS/Methods_loops/script.js
@@ -0,0 +1,13 @@
+let my_str = "I LOVe CoDinG"
+
+document.writeln(my_str.toUpperCase()+"<br>");
+document.writeln(my_str.toLowerCase()+"<br>");
+document.writeln(my_str.startsWith('I LO')+"<br>")
+
+// Number Methods
+
+document.writeln(Math.random())
+document.writeln(`<br>`)
+document.writeln(Math.random()*100)
+document.writeln(`<br>`)
+document.writeln(Math.floor(Math.random()*100))
