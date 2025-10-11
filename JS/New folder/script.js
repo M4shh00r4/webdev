04df5ddd13bfdf45f57152e4f32072fdef960b66
M4shh00r4 <mashhooramanha@gmail.com>
2025-10-11 21:13:46 +0600

    brief JS

---
+++ JS/New folder/script.js
@@ -0,0 +1,31 @@
+let a =10;
+let b =3.7182;
+let c ="Ami string"
+let d =null;
+let e;
+let f =true;
+
+document.writeln(`data type of a=${a} is=${typeof(a)} <br>
+                 data type of a=${b} is=${typeof(b)} <br>
+                 data type of a=${c} is=${typeof(c)} <br>
+                 data type of a=${d} is=${typeof(d)} <br>
+                 data type of a=${e} is=${typeof(e)} <br>
+                 data type of a=${f} is=${typeof(f)} <br>`)
+
+//Conditionals
+function myFunctions(){
+    let time=new Date().getHours();
+    document.getElementById("hour").innerHTML=time
+    if(time >5 && time <12){
+        document.getElementById("greetings").innerHTML=`Good morning`;
+    }
+    else if(time <17){
+        document.getElementById("greetings").innerHTML=`Good afternoon`;
+    }
+    else if(time <20){
+        document.getElementById("greetings").innerHTML=`Good evening`;
+    }
+    else {
+        document.getElementById("greetings").innerHTML=`Good night`;
+    }
+}                 
