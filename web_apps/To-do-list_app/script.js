d4a177442e5967ae2ad305d565039d5bb4d00938
M4shh00r4 <mashhooramanha@gmail.com>
2025-11-08 18:44:46 +0600

    to-do-list

---
+++ web_apps/To-do-list_app/script.js
@@ -0,0 +1,22 @@
+function addTask() {
+     const taskText = document.getElementById('new-task').value.trim();
+     if (taskText === ''){
+        document.getElementById("res").innerHTML = "please enter some text";
+     }
+     const todolist = document.getElementById('todo-list');
+     const li = document.createElement('li');
+     li.className = 'todo-item';
+
+     const taskSpan = document.createElement('span');
+     taskSpan.textContent = taskText;
+
+     const deleteBtn = document.createElement('button')
+     deleteBtn.textContent = 'Delete';
+     deleteBtn.className = 'delete-btn';
+     deleteBtn.onclick= function(){
+        todolist.removeChild(li);
+     };
+     li.appendChild(taskSpan);
+     li.appendChild(deleteBtn);
+     todolist.appendChild(li);
+}
